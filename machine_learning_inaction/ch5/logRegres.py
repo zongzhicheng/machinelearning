9138a332d98402dad19527c39b94632b85c3badb
842117866@qq.com <842117866@qq.com>
2020-12-13 16:01:53 +0800

    zzc

--- machine_learning_inaction/ch5/logRegres.py
+++ machine_learning_inaction/ch5/logRegres.py
@@ -61,8 +61,51 @@ def plotBestFit(weights):
     plt.show()
 
 
+# 随机梯度上升算法
+def stocGradAscent0(dataMatrix, classLabels):
+    m, n = shape(dataMatrix)
+    alpha = 0.01
+    weights = ones(n)  # 初始化每个回归系数为1
+    for i in range(m):
+        h = sigmoid(sum(dataMatrix[i] * weights))
+        error = classLabels[i] - h
+        weights = weights + alpha * error * dataMatrix[i]
+    return weights
+
+
+# 改进的随机梯度上升算法
+def stocGradAscent1(dataMatrix, classLabels, numIter=150):
+    m, n = shape(dataMatrix)
+    weights = ones(n)  # 初始化每个回归系数为1
+    for j in range(numIter):
+        # 书中代码原本是dataIndex = range(m)
+        # python3.x中 range返回的是range对象，不是数组对象
+        # 所以改为list
+        dataIndex = list(range(m))
+        for i in range(m):
+            alpha = 4 / (1.0 + j + i) + 0.0001  # alpha每次迭代时需要调整
+            # 随机选取更新
+            randIndex = int(random.uniform(0, len(dataIndex)))
+            h = sigmoid(sum(dataMatrix[randIndex] * weights))
+            error = classLabels[randIndex] - h
+            weights = weights + alpha * error * dataMatrix[randIndex]
+            del (dataIndex[randIndex])
+    return weights
+
+
 if __name__ == '__main__':
     dataArr, labelMat = loadDataSet()
-    weights = gradAscent(dataArr, labelMat)
+    # 梯度上升算法
+    # weights = gradAscent(dataArr, labelMat)
+    # print(weights)
+    # plotBestFit(weights.getA())
+
+    # 随机梯度上升算法
+    # weights = stocGradAscent0(array(dataArr), labelMat)
+    # print(weights)
+    # plotBestFit(weights)
+
+    # 改进的随机梯度上升算法
+    weights = stocGradAscent1(array(dataArr), labelMat)
     print(weights)
-    plotBestFit(weights.getA())
+    plotBestFit(weights)
