1bbbf8d2d56877a69e95f9438ae69d15c17eb3f5
Zzzzc72 <842117866@qq.com>
2021-01-01 16:47:08 +0800

    zzc

--- machine_learning_inaction/ch02/kNN.py
+++ machine_learning_inaction/ch02/kNN.py
@@ -69,6 +69,34 @@ def autoNum(dataSet):
     return normDataSet, ranges, minVals
 
 
+# 分类器针对约会网站的测试代码
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNum(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m * hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
+        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i]))
+        if (classifierResult != datingLabels[i]):
+            errorCount += 1.0
+        print("the total error rate is: %f" % (errorCount / float(numTestVecs)))
+
+
+def classifyPerson():
+    resultList = ['not at all', 'in small doses', 'in large doses']
+    percentTats = float(input("percentage of time spent playing video games?"))
+    ffMiles = float(input("frequent flier miles earned per year?"))
+    iceCream = float(input("liters of ice cream consumed per year?"))
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNum(datingDataMat)
+    inArr = array([ffMiles, percentTats, iceCream])
+    classifierResult = classify0((inArr - minVals) / ranges, normMat, datingLabels, 3)
+    print("You will probably like this person:", resultList[classifierResult - 1])
+
+
 if __name__ == '__main__':
     group, labels = createDataSet()
     print(group)
@@ -93,3 +121,6 @@ if __name__ == '__main__':
     print(normMat)
     print(ranges)
     print(minVals)
+
+    datingClassTest()
+    classifyPerson()
