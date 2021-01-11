47f9b1bb735662efcad40495b569d485bafea2e2
842117866@qq.com <842117866@qq.com>
2021-01-11 19:55:33 +0800

    zzc

--- machine_learning_inaction/ch03/treePlotter.py
+++ machine_learning_inaction/ch03/treePlotter.py
@@ -8,6 +8,7 @@ leafNode = dict(boxstyle="round4", fc="0.8")
 arrow_args = dict(arrowstyle="<-")
 
 
+# plotNode、createPlot 使用文本注解绘制树节点
 def plotNode(nodeTxt, centerPt, parentPt, nodeType):
     createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction',
                             xytext=centerPt, textcoords='axes fraction',
@@ -21,6 +22,35 @@ def createPlot():
     plotNode('决策节点', (0.5, 0.1), (0.1, 0.5), decisionNode)
     plotNode('叶子节点', (0.8, 0.1), (0.3, 0.8), leafNode)
     plt.show()
+
+
+# 获取叶节点的数目
+def getNumLeafs(myTree):
+    numLeafs = 0
+    firstStr = myTree.keys()[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[
+                    key]).__name__ == 'dict':  # test to see if the nodes are dictonaires, if not they are leaf nodes
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs += 1
+    return numLeafs
+
+
+# 获取树的层数
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = myTree.keys()[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[
+                    key]).__name__ == 'dict':  # test to see if the nodes are dictonaires, if not they are leaf nodes
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else:
+            thisDepth = 1
+        if thisDepth > maxDepth: maxDepth = thisDepth
+    return maxDepth
 
 
 if __name__ == '__main__':
