248146169a14bc4931a23093d7a34aa9dcf58b87
Zzzzc72 <842117866@qq.com>
2021-01-17 00:31:37 +0800

    zzc

--- machine_learning_inaction/ch04/bayes.py
+++ machine_learning_inaction/ch04/bayes.py
@@ -38,9 +38,72 @@ def setOfWords2Vec(vocabList, inputSet):
     return returnVec
 
 
+# 朴素贝叶斯分类器训练函数
+def trainNB0(trainMatrix, trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory) / float(numTrainDocs)
+    # p0Num = zeros(numWords)
+    p0Num = ones(numWords)
+    # p1Num = zeros(numWords)
+    p1Num = ones(numWords)
+    # p0Denom = 0.0
+    p0Denom = 2.0
+    # p1Denom = 0.0
+    p1Denom = 2.0
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            p1Num += trainMatrix[i]
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+    # 对每个元素做除法
+    # p1Vect = p1Num / p1Denom
+    p1Vect = log(p1Num / p1Denom)
+    # p0Vect = p0Num / p0Denom
+    p0Vect = log(p0Num / p0Denom)
+    return p0Vect, p1Vect, pAbusive
+
+
+# 朴素贝叶斯分类函数
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+    p1 = sum(vec2Classify * p1Vec) + log(pClass1)  # 元素相乘
+    p0 = sum(vec2Classify * p0Vec) + log(1.0 - pClass1)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+
+def testingNB():
+    listOPosts, listClasses = loadDataSet()
+    myVocabList = createVocabList(listOPosts)
+    trainMat = []
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+    p0V, p1V, pAb = trainNB0(array(trainMat), array(listClasses))
+    testEntry = ['love', 'my', 'dalmation']
+    thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+    print(testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb))
+    testEntry = ['stupid', 'garbage']
+    thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+    print(testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb))
+
+
 if __name__ == '__main__':
     listOPosts, listClasses = loadDataSet()
     myVocalList = createVocabList(listOPosts)
     print(myVocalList)
     print(setOfWords2Vec(myVocalList, listOPosts[0]))
-    print(setOfWords2Vec(myVocalList, listOPosts[0]))
+    print(setOfWords2Vec(myVocalList, listOPosts[3]))
+
+    # trainMat = []
+    # for postinDoc in listOPosts:
+    #     trainMat.append(setOfWords2Vec(myVocalList, postinDoc))
+    # p0V, p1V, pAb = trainNB0(trainMat, listClasses)
+    # print(pAb)
+    # print(p0V)
+    # print(p1V)
+
+    testingNB()
