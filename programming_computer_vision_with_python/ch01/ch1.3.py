bfd6fba979b905859dad832849012285f82071dd
842117866@qq.com <842117866@qq.com>
2021-03-01 19:14:17 +0800

    zzc

--- programming_computer_vision_with_python/ch01/ch1.3.py
+++ programming_computer_vision_with_python/ch01/ch1.3.py
@@ -54,6 +54,56 @@ def histeq(im, nbr_bins=256):
     return im2.reshape(im.shape), cdf
 
 
+# 计算图像列表的平均图像
+def compute_average(imlist):
+    # 打开第一幅图像，将其存储在浮点型数组中
+    averageim = array(Image.open(imlist[0]), 'f')
+    for imname in imlist[1:]:
+        try:
+            averageim += array(Image.open(imname))
+        except:
+            print(imname + '...skipped')
+    averageim /= len(imlist)
+    # 返回uint8类型的平均图像
+    return array(averageim, 'uint8')
+
+
+# 主成分分析
+def pca(X):
+    """
+    :param X:矩阵X中存储训练数据，每一行为一条训练数据
+    :return:投影矩阵（按照维度的重要性排序）、方差和均值
+    """
+    # 获取维数
+    num_data, dim = X.shape
+
+    # 数据中心化
+    mean_X = X.mean(axis=0)
+    X = X - mean_X
+
+    if dim > num_data:
+        # PCA-使用紧致技巧
+        # 协方差矩阵
+        M = dot(X, X.T)
+        # 特征值和特征向量
+        e, EV = linalg.eigh(M)
+        # 这就是紧致技巧
+        tmp = dot(X.T, EV).T
+        # 由于最后的特征向量是我们所需要的，所以需要将其逆转
+        V = tmp[::-1]
+        # 由于特征值是按照递增顺序排列的，所以需要将其逆转
+        S = sqrt(e)[::-1]
+        for i in range(V.shape[1]):
+            V[:, i] /= S
+    else:
+        # PCA-使用SVD方法
+        U, S, V = linalg.svd(X)
+        # 仅仅返回当前num_data维的数据才合理
+        V = V[:num_data]
+    # 返回投影矩阵、方差和均值
+    return V, S, mean_X
+
+
 if __name__ == '__main__':
     # example1()
     # example2()
