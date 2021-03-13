99c69a8febf97c2b6cd86755a3a579e4545326a8
zongzhicheng <842117866@qq.com>
2021-03-13 19:05:52 +0800

    ch2.1

--- programming_computer_vision_with_python/ch02/ch2.1.py
+++ programming_computer_vision_with_python/ch02/ch2.1.py
@@ -97,6 +97,30 @@ def get_descriptors(image, filtered_coords, wid=5):
     return desc
 
 
+def match(desc1, desc2, threshold=0.5):
+    """
+    对于第一幅图像的每个角点描述子，使用归一化互相关，选取它在第二幅图像中的配角点
+    :param desc1:
+    :param desc2:
+    :param threshold:
+    :return:
+    """
+    n = len(desc1[0])
+
+    # 点对的距离
+    d = -ones((len(desc1), len(desc2)))
+    for i in range(len(desc1)):
+        for j in range(len(desc2)):
+            d1 = (desc1[i] - mean(desc1[i])) / std(desc1[i])
+            d2 = (desc2[j] - mean(desc2[j])) / std(desc2[j])
+            ncc_value = sum(d1 * d2) / (n - 1)
+            if ncc_value > threshold:
+                d[i, j] = ncc_value
+    ndx = argsort(-d)
+    matchscores = ndx[:0]
+    return matchscores
+
+
 if __name__ == '__main__':
     im = array(Image.open('../resource/picture/empire.jpg').convert('L'))
     harrisim = compute_harris_response(im)
